40bf55aa18900f8550aee0d67df1769c95d4a2d9
Michael <mixty01@gmail.com>
2018-11-03 00:12:25 +0600

    Create Task component and set props, create basic structure

---
+++ src/components/Task.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  .task
+    input.completed(
+      type="checkbox"
+      :checked="completed"
+      @change="$emit('change', { completed: $event.target.checked, id })"
+    )
+    input.text(
+      type="text"
+      :value="text"
+      @input="$emit('change', { text: $event.target.value, id })"
+    )
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    completed: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped lang="sass">
+
+</style>
