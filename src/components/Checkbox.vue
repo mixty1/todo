18a398cf6ba576ae02dcae892ab6653650159d57
Michael <mixty01@gmail.com>
2018-11-08 19:38:32 +0600

    Create checkbox component

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,72 @@
+<template lang="pug">
+  .checkbox
+    input.input(
+      type="checkbox"
+      :id="`checkbox-${uuid}`"
+      :checked="checked"
+      @change="$emit('change', $event)"
+    )
+    label.label(:for="`checkbox-${uuid}`")
+</template>
+
+<script>
+import { uniqueId } from 'lodash'
+
+export default {
+  props: {
+    checked: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data: () => ({
+    uuid: uniqueId()
+  })
+}
+</script>
+
+<style scoped lang="sass">
+@import '../assets/styles/_colors'
+@import '../assets/styles/_mixins'
+
+.checkbox
+  position: relative
+  width: 19px
+  height: 19px
+
+  > .input
+    display: none
+
+    &:checked + .label:after
+      border: 1px solid mix(black, $brand-color, 25%)
+
+    &:checked + .label:before
+      opacity: 1
+
+  > .label
+    cursor: pointer
+    width: 100%
+    height: 100%
+    +vh-align
+
+    &:after
+      content: ''
+      background: rgba(#fff, 0.8)
+      width: 100%
+      height: 100%
+      border-radius: 50%
+      border: 1px solid $brand-color
+      +vh-align
+      z-index: 2
+
+    &:before
+      content: ''
+      background: url('../assets/images/check-icon_purple.svg') no-repeat center
+      background-size: 100% 100%
+      width: 80%
+      height: 80%
+      opacity: 0
+      transition: opacity 0.2s
+      +vh-align
+      z-index: 3
+</style>
