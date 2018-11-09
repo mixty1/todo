213b268ac312f86641c150651befd51617af3d87
Michael <mixty01@gmail.com>
2018-11-10 02:02:43 +0600

    Reverse groupped tasks and add task-group class for ungroupped tasks

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -7,13 +7,13 @@
         .todo-list__add(@click="addTask") Добавить
     .todo-list__tasks(v-if="tasks.length")
       .todo-list__task-group(
-        v-for="(taskGroup, date) in groupedTasks"
+        v-for="date in reversedGrouppedTasks"
         :class="{ 'todo-list__task-group--ungroupped': date === 'ungroupped' }"
       )
         .todo-list__date(v-if="date !== 'ungroupped'") {{ date }}
         transition-group(name="tasks-list")
           task.todo-list__task(
-            v-for="task in taskGroup"
+            v-for="task in grouppedTasks[date]"
             :key="task.id"
             :ungroupped="date === 'ungroupped'"
             v-bind="task"
@@ -31,48 +31,51 @@ import _ from 'lodash'
 const emptyTask = {
   id: null,
   text: '',
-  createdAt: null,
+  scheduledAt: null,
   completed: false
 }
 
 export default {
   data: () => ({
     tasks: [
-      // {
-      //   id: '1',
-      //   text: 'Лишить Мишу премии',
-      //   createdAt: new Date(),
-      //   completed: true
-      // },
-      // {
-      //   id: '2',
-      //   text: 'Подготовить презентацию по Гайдми',
-      //   createdAt: new Date(),
-      //   completed: true
-      // },
-      // {
-      //   id: '3',
-      //   text: 'Состряпать бэк для этой тудушки',
-      //   createdAt: new Date(new Date() - 300 * 24 * 60 * 60 * 1000),
-      //   completed: false
-      // },
-      // {
-      //   id: '4',
-      //   text: 'Полистать пикабу',
-      //   createdAt: new Date(new Date() - 2000 - 300 * 24 * 60 * 60 * 1000),
-      //   completed: true
-      // }
+      {
+        id: '1',
+        text: 'Лишить Мишу премии',
+        scheduledAt: new Date(),
+        completed: true
+      },
+      {
+        id: '2',
+        text: 'Подготовить презентацию по Гайдми',
+        scheduledAt: new Date(),
+        completed: true
+      },
+      {
+        id: '3',
+        text: 'Состряпать бэк для этой тудушки',
+        scheduledAt: new Date(new Date() - 300 * 24 * 60 * 60 * 1000),
+        completed: false
+      },
+      {
+        id: '4',
+        text: 'Полистать пикабу',
+        scheduledAt: new Date(new Date() - 2000 - 300 * 24 * 60 * 60 * 1000),
+        completed: true
+      }
     ]
   }),
   computed: {
-    groupedTasks () {
+    grouppedTasks () {
       return _.groupBy(this.tasks, task => {
-        if (task.createdAt) {
-          return this.getDate(task.createdAt)
+        if (task.scheduledAt) {
+          return this.getDate(task.scheduledAt)
         }
 
         return 'ungroupped'
       })
+    },
+    reversedGrouppedTasks () {
+      return _.keys(this.grouppedTasks).reverse()
     }
   },
   methods: {
@@ -92,7 +95,6 @@ export default {
         let task = { ...emptyTask }
 
         task.id = _.uniqueId()
-        // task.createdAt = new Date()
         this.tasks.push(task)
       }
     },
@@ -156,7 +158,8 @@ export default {
 
     &--ungroupped
       background: $main-gradient
-      border-radius: 0 5px 5px 0
+      border-radius: 5px 5px 0 0
+      border-bottom: none
 
       &:last-child
         border-radius: 5px
@@ -168,8 +171,9 @@ export default {
     transition: transform 0.3s, opacity 0.3s
 
   &__date
-    color: #aaa
+    color: #bbb
     font-size: 13px
+    font-weight: bold
 
   &__empty-list
     background: #fff
@@ -196,7 +200,7 @@ export default {
 .tasks-list-enter,
 .tasks-list-leave-to
   opacity: 0
-  transform: translateX(100px)
+  transform: translateY(30px)
 
 .tasks-list-leave-active
   position: absolute
